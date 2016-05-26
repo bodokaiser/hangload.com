c47fd59f571d2f7a92e599c630df2ff46b6b6e50
mrzool <zool.bib@gmail.com>
2016-05-26 17:28:34 +0200

    Move js to separate file

---
+++ js/app.js
@@ -0,0 +1,45 @@
+ // Responsive navigation settings
+  var nav = responsiveNav(".nav-collapse", {
+    customToggle: "nav-toggle",
+    closeOnNavClick: true,
+    openPos: "static"
+  });
+
+  // Show button to continue shopping
+  Snipcart.api.configure('show_continue_shopping', true);
+
+
+  // Display shipping options based on customer address
+
+  var euCountries = [ "AT", "BE", "BG", "HR", "CY", "CZ", "DK", "EE", "FI", "FR", "DE", "GR", "HU", "IE",
+    "IT", "LV", "LT", "LU", "MT", "NL", "PL", "PT", "RO", "SK", "SI", "ES", "SE", "GB" ]
+
+  function setSnipcartShippingMethods(address) {
+    if (address.country == "DE") {
+      Snipcart.api.configure('allowed_shipping_methods', ['germany']);
+    }
+    else if (address.country == euCountries.some()) {
+      Snipcart.api.configure('allowed_shipping_methods', ['eu']);
+    }
+    else {
+      Snipcart.api.configure('allowed_shipping_methods', ['worldwide']);
+    }
+  }
+
+  Snipcart.subscribe('billingaddress.changed', function (address) {
+    if (address.shippingSameAsBilling) {
+      setSnipcartShippingMethods(address);
+    }
+  });
+
+  Snipcart.subscribe('shippingaddress.changed', function (address) {
+    setSnipcartShippingMethods(address);
+  });
+
+  Snipcart.subscribe('cart.ready', function (cart) {
+    if (cart.order) {
+      setSnipcartShippingMethods(cart.order.shippingAddress);
+    }
+  });
+
+
