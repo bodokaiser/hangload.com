f9ded400276fa9e14e60e57a7ee3809bab38e105
mrzool <zool.bib@gmail.com>
2016-06-10 15:29:27 +0200

    Implement FAQ behavior

--- js/app.js
+++ js/app.js
@@ -27,6 +27,16 @@ $(".rslides").responsiveSlides({
   after: function(){}     // Function: After callback
 });
 
+// FAQ-related js
+$('.js-faq-trigger').click(function(e) {
+  // Grab current anchor value
+  var currentAttrValue = $(this).attr('href');
+  // Open up the hidden content panel
+  $(currentAttrValue).toggleClass('display-none'); 
+
+  e.preventDefault();
+});
+
 // Snipcart-related js
 
 // Show button to continue shopping
