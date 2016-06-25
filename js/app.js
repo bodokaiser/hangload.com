94f96a085c37e04c85c1d392f62ad7b5dc1f3a4f
mrzool <zool.bib@gmail.com>
2016-06-25 15:34:00 +0200

    Animate slider automatically

--- js/app.js
+++ js/app.js
@@ -8,8 +8,8 @@ var nav = responsiveNav(".nav-collapse", {
 // Hook up and configure responsive slides plugin
 
 $(".rslides").responsiveSlides({
-  auto: false,             // Boolean: Animate automatically, true or false
-  // speed: 500,            // Integer: Speed of the transition, in milliseconds
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
   // timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
   pager: true,           // Boolean: Show pager, true or false
   nav: false,             // Boolean: Show navigation, true or false
